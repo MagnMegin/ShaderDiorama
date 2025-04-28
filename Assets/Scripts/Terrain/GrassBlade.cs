e965bfad92a0c0ec08a9dc5e245bcd5dec58dcdd
MagnMegin <magne.myran@skomakerhuset.no>
2025-04-29 07:20:18 +1000

    Cleanup and adjustment

--- Assets/Scripts/Terrain/GrassBlade.cs
+++ Assets/Scripts/Terrain/GrassBlade.cs
@@ -19,18 +19,26 @@ public struct GrassBlade
     {
         Mesh mesh = new Mesh();
 
-        Vector3[] vertices = new Vector3[5]
+        Vector3[] vertices = new Vector3[9]
         {
             new Vector3(-0.5f * width, 0f, 0f),
             new Vector3(0.5f * width, 0f, 0f),
-            new Vector3(-0.5f * width, 0.3f * height, 0f),
-            new Vector3(0.5f * width, 0.3f * height, 0f),
+            new Vector3(-0.5f * width, 0.25f * height, 0f),
+            new Vector3(0.5f * width, 0.25f * height, 0f),
+            new Vector3(-0.3f * width, 0.5f * height, 0f),
+            new Vector3(0.3f * width, 0.5f * height, 0f),
+            new Vector3(-0.25f * width, 0.75f * height, 0f),
+            new Vector3(0.25f * width, 0.75f * height, 0f),
             new Vector3(0f, height, 0f),
         };
         
 
-        Vector3[] normals = new Vector3[5]
+        Vector3[] normals = new Vector3[9]
         {
+            Vector3.back,
+            Vector3.back,
+            Vector3.back,
+            Vector3.back,
             Vector3.back,
             Vector3.back,
             Vector3.back,
@@ -38,14 +46,18 @@ public struct GrassBlade
             Vector3.back,
         };
 
-        int[] triangles = new int[9] { 0, 2, 1, 2, 3, 1, 2, 4, 3 };
+        int[] triangles = new int[21] { 0, 2, 1, 2, 3, 1, 2, 4, 3, 3, 4 ,5, 4, 6, 5, 5, 6, 7, 6, 8, 7 };
 
-        Vector2[] uvs = new Vector2[5]
+        Vector2[] uvs = new Vector2[9]
         {
             new Vector2(-0.5f * width, 0f),
             new Vector2(0.5f * width, 0f),
-            new Vector2(-0.5f * width, 0.3f * height),
-            new Vector2(0.5f * width, 0.3f * height),
+            new Vector2(-0.5f * width, 0.25f * height),
+            new Vector2(0.5f * width, 0.25f * height),
+            new Vector2(-0.3f * width, 0.5f * height),
+            new Vector2(0.3f * width, 0.5f * height),
+            new Vector2(-0.25f * width, 0.75f * height),
+            new Vector2(0.25f * width, 0.75f * height),
             new Vector2(0f, height),
         };
 
